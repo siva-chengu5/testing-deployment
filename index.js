a893ad34dd13d0b062c3bf094dff884688783b08
Siva-Chengu <sivakumara.chengubraydoo@umail.uom.ac.mu>
2024-08-04 12:52:05 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Use the connection string from environment variables
+const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://bhuvanesh:bhuvanesh@cluster0.ml8jho1.mongodb.net/?retryWrites=true&w=majority';
+
+// Connect to MongoDB
+mongoose.connect(mongoURI)
+  .then(() => {
+    console.log('Connected to MongoDB Atlas');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB Atlas:', error);
+  });
+
 app.get('/', (req, res) => {
-  res.send('Hello, Heroku!');
+  res.send('Hello, Heroku with MongoDB!');
 });
 
 app.listen(port, () => {
