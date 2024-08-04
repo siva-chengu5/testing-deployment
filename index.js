91630c4241b69f5efd8ccafbd5ffc9cb260d9661
Siva-Chengu <sivakumara.chengubraydoo@umail.uom.ac.mu>
2024-08-04 12:56:05 +0400

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const port = process.env.PORT || 3000;
 // Use the connection string from environment variables
 const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://bhuvanesh:bhuvanesh@cluster0.ml8jho1.mongodb.net/?retryWrites=true&w=majority';
 
+// Set strictQuery option to avoid deprecation warning
+mongoose.set('strictQuery', true);
+
 // Connect to MongoDB
 mongoose.connect(mongoURI)
   .then(() => {
